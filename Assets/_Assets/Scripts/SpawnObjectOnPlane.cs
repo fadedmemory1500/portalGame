9e255931758c4145a5c8abce8d6d1acfd4a42c78
YingC <fadedmemory@gmail.com>
2022-04-16 00:47:24 -0700

    spawn one script

--- Assets/_Assets/Scripts/SpawnObjectOnPlane.cs
+++ Assets/_Assets/Scripts/SpawnObjectOnPlane.cs
@@ -8,32 +8,67 @@ using UnityEngine.XR.ARSubsystems;
 
 public class SpawnObjectOnPlane : MonoBehaviour
 {
-
+    //store reference to raycast manager
     private ARRaycastManager raycastManager;
+
+    //keep track of spwaned object reference
     private GameObject spawnedObject;
 
     [SerializeField]
     private GameObject PlaceablePrefab;
 
-    static List<ARRaycastHit> s_Hits = new list,ARRaycastHit>();
+    //store list of raycast hits
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
 
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    private void Awake()
     {
-        = new List<ARRaycastHit>();
+        //fill the reference to the raycast manager
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
-
-
-
-    void Start()
+    //recording whether the user is touch the screen, what to do if it's a valid touch
+    bool TryGetTouchPostion(out Vector2 touchPosition)
     {
-        
+        //check the touch is more than 0 (touch is valid)
+        if(Input.touchCount > 0)
+        {
+            //if it is a valid touch, store the postion into input.gettouch.position
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        //if now the valid touch, it will set the position to default and return
+        touchPosition = default;
+        return false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //check if we have valid TryGetTouchPostion
+        //if not touch, return
+        if(!TryGetTouchPostion(out Vector2 touchPosition))
+        {
+            return;
+        }
         
+        if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            //store a variable called hitPos; first of the list
+            var hitPose = s_Hits[0].pose;
+
+            //check if we have spawn any ojbect yet
+            if(spawnedObject == null)
+            {
+                spawnedObject = Instantiate(PlaceablePrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation = hitPose.rotation;
+            }
+
+        }
     }
 }
