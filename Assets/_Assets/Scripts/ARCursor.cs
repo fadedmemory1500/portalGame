0ee4c1ceaacc4056810a10e0c0d65f68c2190e35
YingC <fadedmemory@gmail.com>
2022-04-15 17:32:33 -0700

    arcursor script update

--- Assets/_Assets/Scripts/ARCursor.cs
+++ Assets/_Assets/Scripts/ARCursor.cs
@@ -7,19 +7,15 @@ public class ARCursor : MonoBehaviour
 {
     public GameObject cursorChildObject;
     public GameObject objectToPlace;
-
-    //rayCastManager=detects planes we touch
     public ARRaycastManager raycastManager;
 
-    //toggles cursor visibility
-    public bool useCursor;
+    public bool useCursor = true;
 
     void Start()
     {
-        cursorChildObject.SetActive(useCursor); 
+        cursorChildObject.SetActive(useCursor);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (useCursor)
@@ -27,22 +23,16 @@ public class ARCursor : MonoBehaviour
             UpdateCursor();
         }
 
-        //check if that's the beginning of a touch not user holding down a touch
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            //check if it's using a cursor
             if (useCursor)
             {
-                //if cursor is use=true, just instantiate at cursor position
                 GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
             }
             else
             {
-                //if cursor is not use, do another raycast at the touch position
                 List<ARRaycastHit> hits = new List<ARRaycastHit>();
                 raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-                
-                //if hit count greater than 0, instantiate at the hit postition and rotation
                 if (hits.Count > 0)
                 {
                     GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
@@ -53,7 +43,6 @@ public class ARCursor : MonoBehaviour
 
     void UpdateCursor()
     {
-        //mid of 1 location
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
