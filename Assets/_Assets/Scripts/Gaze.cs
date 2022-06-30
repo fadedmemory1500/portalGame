c5fa56f2c5970fd3ded262eeaa8225bb156f3718
YingC <fadedmemory@gmail.com>
2022-06-30 00:16:29 -0700

    gaze script

---
+++ Assets/_Assets/Scripts/Gaze.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    void Undate()
+    {
+        if (Physics.Raycast(transform.position,transform.forward, out RaycastHit hit)){
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                print("HERE");
+                OpenInfo(go.GetComponent<InfoBehavior>());
+            }
+            else
+            {
+                CloseAll();
+            }
+            
+        }
+    }
+
+    void OpenInfo(InfoBehavior desiredInfo)
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            if (info == desiredInfo)
+            {
+                info.OpenInfo();
+            } else
+            {
+                info.CloseInfo();
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+        }
+    }
+
+}
