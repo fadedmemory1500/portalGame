d9a16bfd9768a012bd4b2a0213bab2f0e10a3717
YingC <fadedmemory@gmail.com>
2022-06-29 19:41:11 -0700

    AR Gaze interactions

---
+++ Assets/_Assets/Scripts/InfoBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBehavior : MonoBehaviour
+{
+    const float SPEED = 6f;
+    [SerializeField]
+
+    //that's what I going to scale
+    Transform SectionInfo;
+
+    //what I going to interpolate. default the info panel to 0 so it's off at start
+    Vector3 desiredScale = Vector3.zero;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        SectionInfo.localScale = Vector3.Lerp(SectionInfo.localScale, desiredScale, Time.deltaTime * SPEED);
+    }
+
+    public void OpenInfo()
+    {
+        desiredScale = Vector3.one;
+    }
+
+    public void CloseInfo()
+    {
+        desiredScale = Vector3.zero;
+    }
+}
