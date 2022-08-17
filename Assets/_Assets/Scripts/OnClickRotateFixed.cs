28dd4c7d4f1f156d0d5e1bb9a282eb88d59d4f15
YingC <fadedmemory@gmail.com>
2022-08-17 16:39:05 -0700

    rotate camera change to rotate scene objs

---
+++ Assets/_Assets/Scripts/OnClickRotateFixed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnClickRotateFixed : MonoBehaviour
+{
+    public float rotationAmount;
+    private Quaternion currentRotation; // stores a value for rotation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    public void RotateObjectLeft()
+    {
+        currentRotation = transform.rotation;
+        transform.rotation = currentRotation * Quaternion.Euler(0, rotationAmount, 0); // updates the y rotation of the object
+    }
+
+    public void RotateObjectRight()
+    {
+        currentRotation = transform.rotation;
+        transform.rotation = currentRotation * Quaternion.Euler(0, -rotationAmount, 0); // updates the y rotation of the object
+    }
+}
---
+++ Assets/_Assets/Scripts/OnTouchAudioPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouchAudioPlay : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        audioSource.Play();
+    }
+}
